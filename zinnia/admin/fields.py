5b69eabb8815050a1c0d396fd384bc469e6836cc
Gert-Jan Braas <braas@steckelfisch.nl>
2024-02-16 10:54:15 +0000

    Update fields.py
    Conform to Django 5

--- zinnia/admin/fields.py
+++ zinnia/admin/fields.py
@@ -45,3 +45,5 @@ class MPTTModelMultipleChoiceField(forms.ModelMultipleChoiceField):
          Override the choices getter method to use MPTTModelChoiceIterator.
          """
         return MPTTModelChoiceIterator(self)
+
+    choices = property(_get_choices, forms.ChoiceField._set_choices)
